8dddeeec7fc5fb67f78bce441e7160dd76b986e3
meghana <idired45@students.rowan.edu>
2023-04-12 17:49:24 -0400

    Initial Revision

---
+++ kernel.c
@@ -0,0 +1,18 @@
+#include "console.h"
+void main() 
+{
+	clear_terminal();
+	print_character('H');
+	print_string("ello");
+	print_line("world");
+	print_string("TODAY");
+	return;
+}
+
+
+
+
+
+
+
+
